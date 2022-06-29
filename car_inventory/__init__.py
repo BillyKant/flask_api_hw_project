7288fc0ab74a1ffcb4391b63eda3926a29395d8f
BillyKant <105659514+BillyKant@users.noreply.github.com>
2022-06-28 21:57:50 -0400

    hw day 2

--- car_inventory/__init__.py
+++ car_inventory/__init__.py
@@ -1,7 +1,13 @@
+from cgi import MiniFieldStorage
 from flask import Flask
 from .site.routes import site
 from .authentication.routes import auth
 from config import Config
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
+
+from .models import db as root_db
 
 app = Flask(__name__)
 
@@ -9,3 +15,7 @@ app.register_blueprint(site)
 app.register_blueprint(auth)
 
 app.config.from_object(Config)
+
+root_db.init_app(app)
+
+migrate = Migrate(app, root_db)
--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'YOU WILL NEVER GUESS IT'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATION = False # turns off updates messages from sqlalchemy
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # turns off updates messages from sqlalchemy
 
