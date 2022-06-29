dfa6ac04e42ee3e9fb8bdf8a8fb55d5990620b82
BillyKant <105659514+BillyKant@users.noreply.github.com>
2022-06-29 10:13:40 -0400

    renamed config var to match with env

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class Config():
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'YOU WILL NEVER GUESS IT'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # turns off updates messages from sqlalchemy
 
